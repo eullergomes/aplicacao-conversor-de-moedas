492e5dd353ebb14bf77d1b3ba21dd7259c143ab4
eullergomes <eullertexeira@gmail.com>
2024-04-22 20:47:25 -0300

    feat: dynamic data rendering with API

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-/*
-  - Construa uma aplicação de conversão de moedas. O HTML e CSS são os que você
-    está vendo no browser;
-  - Você poderá modificar a marcação e estilos da aplicação depois. No momento, 
-    concentre-se em executar o que descreverei abaixo;
-    - Quando a página for carregada: 
-      - Popule os <select> com tags <option> que contém as moedas que podem ser
-        convertidas. "BRL" para real brasileiro, "EUR" para euro, "USD" para 
-        dollar dos Estados Unidos, etc.
-      - O option selecionado por padrão no 1º <select> deve ser "USD" e o option
-        no 2º <select> deve ser "BRL";
-      - O parágrafo com data-js="converted-value" deve exibir o resultado da 
-        conversão de 1 USD para 1 BRL;
-      - Quando um novo número for inserido no input com 
-        data-js="currency-one-times", o parágrafo do item acima deve atualizar 
-        seu valor;
-      - O parágrafo com data-js="conversion-precision" deve conter a conversão 
-        apenas x1. Exemplo: 1 USD = 5.0615 BRL;
-      - O conteúdo do parágrafo do item acima deve ser atualizado à cada 
-        mudança nos selects;
-      - O conteúdo do parágrafo data-js="converted-value" deve ser atualizado à
-        cada mudança nos selects e/ou no input com data-js="currency-one-times";
-      - Para que o valor contido no parágrafo do item acima não tenha mais de 
-        dois dígitos após o ponto, você pode usar o método toFixed: 
-        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-    - Para obter as moedas com os valores já convertidos, use a Exchange rate 
-      API: https://www.exchangerate-api.com/;
-      - Para obter a key e fazer requests, você terá que fazer login e escolher
-        o plano free. Seus dados de cartão de crédito não serão solicitados.
-*/
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+//div onde será inserido o aviso de erro
+const currenciesEL = document.querySelector('[data-js="currencies-container"]');
+
+const getErrorMessage = errorType => ({
+  'unsupported-code': 'a moeda não existe em nosso banco de dados',
+  'malformed-request': 'a solicitação de conversão não é válida',
+  'invalid-key': 'a chave da API não é válida',
+  'inactive-account': 'a conta está inativa',
+  'quota-reached': 'a cota de solicitações foi atingida',
+})[errorType] || 'não foi possível obter as informações'
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch('https://v6.exchangerate-api.com/v6/5861b7ff3ae03b0fc88cc183/latest/USD');
+    const exchangeRateData = await response.json();
+
+    //se request mal sucedida
+    if (!response.ok) {
+      throw new Error ('Sua conexão falhou. Não foi possível obter as informações.');
+    }
+
+    if (exchangeRateData.result === 'error') {
+      throw new Error(getErrorMessage(exchangeRateData['error-type']));
+    }
+
+    return exchangeRateData;
+  } catch (error) {
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+
+    div.textContent = error.message;
+    div.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show');
+    div.setAttribute('role', 'alert')
+    button.classList.add('btn-close');
+    button.setAttribute('type', 'button');
+    button.setAttribute('aria-label', 'Close');
+
+    button.addEventListener('click', () => {
+      div.remove();
+    })
+
+    div.appendChild(button)
+    currenciesEL.insertAdjacentElement('afterend', div);
+
+    console.log(div);
+    /*
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      Mensagem de erro
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    */
+  }
+}
+
+//initialize
+const init = async () => {
+  const exchangeRateData = await fetchExchangeRate();
+
+  const getOptions = selectedCurrency => Object.keys(exchangeRateData.conversion_rates)
+    .map(currency => `<option ${currency === selectedCurrency ? 'selected' : ''}>${currency}</option>`)
+    .join('')
+
+  currencyOneEl.innerHTML = getOptions('USD');
+  currencyTwoEl.innerHTML = getOptions('BRL')
+}
+
+init();
